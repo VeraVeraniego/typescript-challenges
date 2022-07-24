f34835722146d1a9aeae4dc122afdb0395f4c687
Brayan Vera <brayanvera@ravn.co>
2022-07-23 19:47:15 -0500

    pick challenge done

--- questions/4-easy-pick/template.ts
+++ questions/4-easy-pick/template.ts
@@ -1,3 +1,2 @@
 // export type MyPick<DataType, KeyType extends keyof DataType>=Pick<DataType,KeyType>
-
 export type MyPick<DataType, KeyType extends keyof DataType> =  {[key in KeyType]:DataType[key]}
