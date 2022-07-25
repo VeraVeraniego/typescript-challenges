5f651862a428d0be6d09e7fe037dd06f7444f3a7
Brayan Vera <brayanvera@ravn.co>
2022-07-24 21:45:31 -0500

    includes done

--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,3 @@
-export type Includes<T extends readonly any[], U> = any
+type IsEqual<A, B> = [A, B] extends [B, A] ? true : false 
+export type Includes<T extends readonly any[], U> = T extends [infer X, ...infer Rest] ?
+IsEqual<U, X> extends true ? true : Includes<Rest, U>: false
