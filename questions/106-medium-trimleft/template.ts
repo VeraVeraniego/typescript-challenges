adca8cd166fad525fee2f32d00a82d445a55991a
Brayan Vera <BrayanVera@ravn.co>
2022-07-24 23:37:49 -0500

    trimleft done

--- questions/106-medium-trimleft/template.ts
+++ questions/106-medium-trimleft/template.ts
@@ -1,1 +1,3 @@
-export type TrimLeft<S extends string> = any
+import { Trim } from "../108-medium-trim/template";
+type Empty=" " | "\n" | "\t"
+export type TrimLeft<S extends string> = S extends`${Empty}${infer Rest}` ? TrimLeft<Rest>: S
