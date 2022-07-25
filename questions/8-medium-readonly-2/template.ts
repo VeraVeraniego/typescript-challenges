346b8ab271da7b5b40c8a7d3788e4273e89c1fd7
Brayan Vera <brayanvera@ravn.co>
2022-07-24 22:18:42 -0500

    readonly2 done

--- questions/8-medium-readonly-2/template.ts
+++ questions/8-medium-readonly-2/template.ts
@@ -1,1 +1,1 @@
-export type MyReadonly2<T, K extends keyof T = keyof T> = {readonly [k in keyof T] : T[k];} & Omit<T,K>
+export type MyReadonly2<T, K extends keyof T = keyof T> = Readonly<Pick<T, K>> & Omit<T, K>
